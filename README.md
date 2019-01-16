51f42427123354ddb567bd5ba2a396f177bcf19d
Saket Tawde <saket.tawde@gmail.com>
2019-01-16 13:12:12 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-apps
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,85 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter, Route } from "react-router-dom"
+import Contacts from "./screens/Contacts"
+import AddContact from "./screens/AddContact"
+import View from "./screens/View"
+import Edit from "./screens/Edit"
+// import {Container} from "semantic-ui-react"
+import { Container } from "./comps/styles"
+
+import "./styles.css"
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    idToAssign: 1
+  }
+
+  addContact = newContact => {
+    let newContacts = this.state.contacts
+    newContact.id = this.state.idToAssign
+    newContacts.push(newContact)
+    console.log(newContacts)
+    this.setState({
+      contacts: newContacts,
+      idToAssign: this.state.idToAssign + 1
+    })
+  }
+
+  editContact = newContact => {
+    const id = newContact.id
+    const newContacts = this.state.contacts.map(item => {
+      if (item.id === id) {
+        return newContact
+      }
+      return item
+    })
+    this.setState({
+      contacts: newContacts
+    })
+  }
+
+  render() {
+    return (
+      <Container>
+        <Route
+          path="/"
+          render={routeProps => (
+            <div>
+              <h2>Contact Manager Landing Page</h2>
+            </div>
+          )}
+          exact
+        />
+        <Route
+          path="/contacts"
+          render={routeProps => (
+            <Contacts contacts={this.state.contacts} {...routeProps} />
+          )}
+        />
+        <Route
+          path="/add"
+          render={routeProps => (
+            <AddContact addContact={this.addContact} {...routeProps} />
+          )}
+        />
+        <Route
+          path="/edit"
+          render={routeProps => (
+            <Edit editContact={this.editContact} {...routeProps} />
+          )}
+        />
+        <Route path="/view" render={routeProps => <View {...routeProps} />} />
+      </Container>
+    )
+  }
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+)
